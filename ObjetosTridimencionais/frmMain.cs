f82966c0996e894175476fead4fe571e0bc1e2aa
Aluno2 <Aluno2@LF103M02-69337>
2019-10-10 08:36:15 -0300

    teste

---
+++ ObjetosTridimencionais/frmMain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ObjetosTridimencionais
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            pbCanvas.Size = new Size((int)(this.Size.Width * 0.8), this.Size.Height);
+        }
+    }
+}
